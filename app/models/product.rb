da0d9c59f6a728ff7e1270be18841024abe06120
flymin <gaoruiyuan@buaa.edu.cn>
2018-12-26 16:55:12 +0800

    cart part one finish

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,20 @@ class Product < ApplicationRecord
     with:    %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+  has_many :line_items
+ 
+  before_destroy :ensure_not_referenced_by_any_line_item
+ 
+  #...
+ 
+ 
+  private
+ 
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
   root 'store#index', as: 'store_index'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

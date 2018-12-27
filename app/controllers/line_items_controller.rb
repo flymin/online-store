611e0e4cbd49fa8256b75717459c528534128ac6
flymin <gaoruiyuan@buaa.edu.cn>
2018-12-27 21:01:22 +0800

    test pass

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -34,6 +34,7 @@ class LineItemsController < ApplicationController
       if @line_item.save
         #format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
         format.html { redirect_to @line_item.cart }
+        format.js   { @current_item = @line_item }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }

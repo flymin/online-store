698444ce0abff4b4d03ff8bba42d2a143e4a2c1f
flymin <gaoruiyuan@buaa.edu.cn>
2018-12-27 23:19:53 +0800

    add channel and fix nil bug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # 为Channel添加，使得远端能够访问服务器
+  config.action_cable.disable_request_forgery_protection = true
 end

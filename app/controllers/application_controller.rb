68f2e7e5f4366d1fae69d5b2e4b3870d3e3cc0f8
Flymin <gaoruiyuan@buaa.edu.cn>
2018-12-29 23:00:37 +0800

    add errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,32 @@ class ApplicationController < ActionController::Base
   before_action :authorize
 
   # ...
-  rescue_from 'User::Error' do |exception|
-    if controller_name == "users" && action_name == "delete"
-      redirect_to users_url, notice: exception.message
-    elsif controller_name == "categories" && action_name == "delete"
-      redirect_to categories_url, notice: exception.message
-    else
-      render plain: 'Not found', status: 400
-    end
+  def self.rescue_errors
+   	rescue_from 'User::Error' do |exception|
+			if controller_name == "users" && action_name == "delete"
+				redirect_to users_url, notice: exception.message
+			elsif controller_name == "categories" && action_name == "delete"
+				redirect_to categories_url, notice: exception.message
+			else
+				render plain: 'Not found', status: 400
+			end
+		end
+ 		rescue_from Exception, :with => :render_error
+    rescue_from RuntimeError, :with => :render_error
+    rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
+    rescue_from ActionController::RoutingError, :with => :render_not_found
+    rescue_from ActionController::UnknownController, :with => :render_not_found
+    rescue_from ActionController::UnknownAction, :with => :render_not_found
+  end
+
+  rescue_errors unless Rails.env.development?
+
+  def render_not_found(exception = nil)
+    render :file => "/public/404.html", :status => 404
+  end
+
+  def render_error(exception = nil)
+    render :file => "/public/500.html", :status => 500
   end
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,7 @@ Rails.application.routes.draw do
     get :who_bought, on: :member
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '*path' => proc { |env| Rails.env.development? ?
+				(raise ActionController::RoutingError, %{No route matches "#{env["PATH_INFO"]}"}) : 
+				ApplicationController.action(:render_not_found).call(env) }
 end

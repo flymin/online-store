917b931cc485c214eb8fc5245bacf3e30a58ad0b
Flymin <gaoruiyuan@buaa.edu.cn>
2018-12-29 23:27:47 +0800

    want to push

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
+  # Show full error reports.
   config.consider_all_requests_local = false
 
   # Enable/disable caching. By default caching is disabled.
